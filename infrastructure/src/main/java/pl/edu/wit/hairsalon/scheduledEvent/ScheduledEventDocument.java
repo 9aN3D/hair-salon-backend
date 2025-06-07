ea448c0a1bcb5527614627aa5477e430d43588cc
Volodymyr Kovalchuk <9an3dw0w@gmail.com>
2025-06-07 11:34:17 +0200

    Migrate to Spring boot 3.x v2

--- infrastructure/src/main/java/pl/edu/wit/hairsalon/scheduledEvent/ScheduledEventDocument.java
+++ infrastructure/src/main/java/pl/edu/wit/hairsalon/scheduledEvent/ScheduledEventDocument.java
@@ -5,8 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.redis.core.index.Indexed;
 import pl.edu.wit.hairsalon.scheduledEvent.dto.ScheduledEventTypeDto;
 import pl.edu.wit.hairsalon.sharedKernel.dto.DateRangeDto;
 
